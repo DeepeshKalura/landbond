7ce8546bbb2ef26f27c7b4c45c54284830a525ca
DeepeshKalura <deepeshkalurs@gmail.com>
2024-10-24 22:44:07 +0530

    firebase configure complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'firebase_options.dart';
 import 'core/app_routes.dart';
 import 'core/bloc_observator.dart';
 import 'presentation/auth/bloc/auth_bloc.dart';
 
-void main() {
+Future<void> main() async {
   Bloc.observer = BlocObservationLogger();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
