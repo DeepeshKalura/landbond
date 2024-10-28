16c2775d9bc62ae50eb4556a19c57a62f8483378
DeepeshKalura <deepeshkalurs@gmail.com>
2024-10-28 13:56:01 +0530

    update routes to clear old design and infastructural

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -4,12 +4,8 @@ import 'package:go_router/go_router.dart';
 import '/core/app_url.dart';
 // screens
 import '../presentation/auth/screen/signin_screen.dart';
-import '../presentation/auth/oldscreen/opt_verfication_screen.dart';
-import '../presentation/auth/oldscreen/profile_regiseration_screen.dart';
-import '../presentation/auth/oldscreen/profile_type_screen.dart';
-import '../presentation/auth/oldscreen/signup_screen.dart';
-import '../presentation/auth/oldscreen/welcome_screen.dart';
 import '../presentation/home/screen/home_screen.dart';
+import '../presentation/onboading/onboarding_screen.dart';
 import '../presentation/splash/splash_screen.dart';
 
 final rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -25,39 +21,19 @@ class AppRoutes {
         builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
-        path: '/welcome',
-        name: AppUrl.welcomeScreen,
-        builder: (context, state) => const WelcomeScreen(),
-      ),
-      GoRoute(
         path: '/signin',
         name: AppUrl.signInScreen,
         builder: (context, state) => const SignInScreen(),
       ),
       GoRoute(
-        path: '/signup',
-        name: AppUrl.signUpScreen,
-        builder: (context, state) => const SignUpScreen(),
-      ),
-      GoRoute(
-        path: '/optVerfication',
-        name: AppUrl.optVerficationScreen,
-        builder: (context, state) => const OptVerficationScreen(),
-      ),
-      GoRoute(
-        path: "/profileRegiseration",
-        name: AppUrl.profileRegiserationScreen,
-        builder: (context, state) => const ProfileRegistrationScreen(),
-      ),
-      GoRoute(
-        path: "/profileType",
-        name: AppUrl.profileTypeScreen,
-        builder: (context, state) => const ProfileTypeScreen(),
-      ),
-      GoRoute(
         path: '/home',
         name: AppUrl.homeScreen,
         builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/onboarding',
+        name: AppUrl.onboardingScreen,
+        builder: (context, state) => const OnboardingScreen(),
       )
     ],
   );
--- lib/core/app_url.dart
+++ lib/core/app_url.dart
@@ -1,10 +1,8 @@
 class AppUrl {
+  // Currently used
   static const String splashScreen = "splashScreen";
   static const String homeScreen = "homeScreen";
   static const String signInScreen = "signInScreen";
-  static const String welcomeScreen = "welcomeScreen";
   static const String signUpScreen = "signUpScreen";
-  static const String optVerficationScreen = "optVerficationScreen";
-  static const String profileRegiserationScreen = "profileRegiserationScreen";
-  static const String profileTypeScreen = 'profileTypeScreen';
+  static const String onboardingScreen = 'onboardingScreen';
 }
