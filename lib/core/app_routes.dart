f58bac954f9af69a78861a4af8638023547f90d1
DeepeshKalura <deepeshkalurs@gmail.com>
2024-11-03 07:58:28 +0530

    application routers

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../presentation/onboading/next_boading_screen.dart';
+import '../presentation/auth/screen/confirm_email_screen.dart';
+import '../presentation/auth/screen/email_verified_screen.dart';
+import '../presentation/auth/screen/new_password_screen.dart';
 import '/core/app_url.dart';
 // screens
+import '../presentation/auth/screen/checking_email_forgot_password_screen.dart';
+import '../presentation/auth/screen/clinet_signin_screen.dart';
+import '../presentation/auth/screen/forgot_password_screen.dart';
 import '../presentation/auth/screen/signin_screen.dart';
 import '../presentation/home/screen/home_screen.dart';
+import '../presentation/auth/screen/client_registeration_screen.dart';
+import '../presentation/onboading/next_boading_screen.dart';
 import '../presentation/onboading/onboarding_screen.dart';
 import '../presentation/splash/splash_screen.dart';
 
@@ -40,6 +47,41 @@ class AppRoutes {
         path: '/nextboarding',
         name: AppUrl.nextBoardingScreen,
         builder: (context, state) => const NextboardingScreen(),
+      ),
+      GoRoute(
+        path: '/clinetsignup',
+        name: AppUrl.clientSignupScreen,
+        builder: (context, state) => const ClientSignupScreen(),
+      ),
+      GoRoute(
+        path: '/loginUser',
+        name: AppUrl.loginUserScreen,
+        builder: (context, state) => const ClientSigninScreen(),
+      ),
+      GoRoute(
+        path: '/forgotPassword',
+        name: AppUrl.forgotPasswordScreen,
+        builder: (context, state) => const ForgotPasswordScreen(),
+      ),
+      GoRoute(
+        path: '/checkingEmailForgotPassword',
+        name: AppUrl.checkingEmailForgotPasswordScreen,
+        builder: (context, state) => const CheckingEmailForgotPasswordScreen(),
+      ),
+      GoRoute(
+        path: '/emailVerify',
+        name: AppUrl.emailVerifiedScreen,
+        builder: (context, state) => const EmailVerifiedScreen(),
+      ),
+      GoRoute(
+        path: '/newPassword',
+        name: AppUrl.newPasswordScreen,
+        builder: (context, state) => const NewPasswordScreen(),
+      ),
+      GoRoute(
+        path: '/confirmEmailScreen',
+        name: AppUrl.confirmEmailScreen,
+        builder: (context, state) => const ConfirmEmailScreen(),
       ),
     ],
   );
--- lib/core/app_url.dart
+++ lib/core/app_url.dart
@@ -3,7 +3,15 @@ class AppUrl {
   static const String splashScreen = "splashScreen";
   static const String homeScreen = "homeScreen";
   static const String signInScreen = "signInScreen";
-  static const String signUpScreen = "signUpScreen";
   static const String onboardingScreen = 'onboardingScreen';
   static const String nextBoardingScreen = 'nextBoardingScreen';
+  static const String clientSignupScreen = 'clientSignupScreen';
+  static const String loginUserScreen = 'loginUserScreen';
+  static const String forgotPasswordScreen = 'forgotPasswordScreen';
+  static const String checkingEmailForgotPasswordScreen =
+      'checkingEmailForgotPasswordScreen';
+
+  static const String emailVerifiedScreen = 'emailVerifiedScreen';
+  static const String confirmEmailScreen = 'confirmEmailScreen';
+  static const String newPasswordScreen = 'newPasswordScreen';
 }
